34a0b574dbff39d5e4b9a5fb8a83994cb1261563
Nitish_Kumar <nishkumar562@gmail.com>
2024-02-06 22:53:04 +0530

    components add

--- nitish_kumar/src/App.js
+++ nitish_kumar/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="circle">
+        <div className='name-tag'>I'm Nitish</div>
+      </div >
+      <div className="Items">
+        <div id="mercury-orbit">
+          <div id="mercury" ></div>
+        </div>
+
+        <div id="venus-orbit">
+          <div id="venus"></div>
+        </div>
+
+        <div id="earth-orbit">
+          <div id="earth" ></div>
+
+        </div>
+      </div>
+
+      <div className='info'>
+        This is for my information...<br></br>
+        This is for my information...<br></br>
+        This is for my information...<br></br>
+        This is for my information...
+      </div>
     </div>
   );
 }
